4e331a7b208978e940a26d1ea3e9c948577528c4
Edward R <edwardrz@users.noreply.github.com>
2017-07-11 20:30:38 -0500

    Completed loops: 1-increment, 2-decrement.

---
+++ js/main.js
@@ -0,0 +1,38 @@
+"use strict";
+console.log("main.js");
+
+
+// INCREMENT BY 10.
+// Starts at 5, adds 10 and stops when the value is over 120.
+console.log("**************************");
+console.log("INCREMENT BY 10")
+console.log("**************************");
+
+for (let i = 5; i < 120; i = i + 10) {
+console.log("Current value is ", i);
+}
+
+
+
+//DECREMENT BY DIVISION
+//A For loop reducing 4096 by dividing by two at each step.
+
+console.log("**************************");
+console.log("DECREMENT BY DIVISION");
+console.log("**************************");
+
+for (let i = 4096; i > 1; i = i / 2) {
+	console.log("Current value is ", i);
+}
+
+
+//ARRAY INTERATION
+
+
+
+//OBJECT ITERATION
+
+
+
+
+
